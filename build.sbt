f6e8218c3469decf1dc98b1a8b3fb6da22e419f1
yash-bhardwaj-22 <yash.bhardwaj@knoldus.com>
2021-11-02 22:44:29 +0530

    Initial Commit

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "ScalaStudio-Assignment10"
+
+version := "0.1"
+
+scalaVersion := "2.13.7"
---
+++ src/main/scala/HeadAndTail.scala
@@ -0,0 +1,13 @@
+import scala.::
+
+class HeadAndTail {
+  def isort(xs: List[Int]): List[Int] = {
+    if (xs.isEmpty) Nil
+    else insert(xs.head, isort(xs.tail))
+  }
+
+  def insert(x: Int, xs: List[Int]):List[Int] = {
+    if (xs.isEmpty || x <= xs.head) x :: xs
+    else xs.head :: insert(x, xs.tail)
+  }
+}
